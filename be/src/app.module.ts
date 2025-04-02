c78b28da39d610a172ff8209d2f12397d98c1737
mmingyeomm <aliveck0208@gmail.com>
2025-04-02 20:49:33 +0900

    be_modules

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
     }),
     AuthModule,
-    UserModule, 
+    UserModule,
+    ChatModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
